6a219020f553a9b4d0295df6601ff28120a8fda3
Dyneteve <dyneteve@hentaios.com>
2021-09-08 12:41:45 +0200

    Merge tag 'android-11.0.0_r43' of https://android.googlesource.com/platform/frameworks/base into HEAD
    Android 11.0.0 release 43
    
    * tag 'android-11.0.0_r43' of https://android.googlesource.com/platform/frameworks/base:
      Use IntentFilter CREATOR directly for serializing ParsedIntentInfo
      Don't export HeapDumpProvider.
      Don't attach private Notification to A11yEvent when user locked
      Merge "BG-FGS-start while-in-use permission restriction improvement." into rvc-dev am: e51f884f6a
      Avoid locking profile task when it is already lock
      Improve ellipsize performance
      Fix side effects of trace-ipc and dumpheap commands
      DO NOT MERGE Add cross-user check for getDefaultSmsPackage().
      Remove ParsedIntentInfo CREATOR
      Fix race condition between lockNow() and updateLockscreenTimeout

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -612,6 +612,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private volatile int mTopFocusedDisplayId = INVALID_DISPLAY;
 
     private int mPowerButtonSuppressionDelayMillis = POWER_BUTTON_SUPPRESSION_DELAY_DEFAULT_MILLIS;
+    
+    private boolean mLockNowPending = false;
 
     private final List<DeviceKeyHandler> mDeviceKeyHandlers = new ArrayList<>();
 
@@ -4998,6 +5000,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mKeyguardDelegate.doKeyguardTimeout(options);
                 }
                 mLockScreenTimerActive = false;
+                mLockNowPending = false;
                 options = null;
             }
         }
@@ -5007,7 +5010,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    ScreenLockTimeout mScreenLockTimeout = new ScreenLockTimeout();
+    final ScreenLockTimeout mScreenLockTimeout = new ScreenLockTimeout();
 
     @Override
     public void lockNow(Bundle options) {
@@ -5019,6 +5022,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mScreenLockTimeout.setLockOptions(options);
         }
         mHandler.post(mScreenLockTimeout);
+        synchronized (mScreenLockTimeout) {
+            mLockNowPending = true;
+        }
     }
 
     // TODO (b/113840485): Move this logic to DisplayPolicy when lockscreen supports multi-display.
@@ -5034,6 +5040,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void updateLockScreenTimeout() {
         synchronized (mScreenLockTimeout) {
+            if (mLockNowPending) {
+                Log.w(TAG, "lockNow pending, ignore updating lockscreen timeout");
+                return;
+            }
             final boolean enable = !mAllowLockscreenWhenOnDisplays.isEmpty()
                     && mDefaultDisplayPolicy.isAwake()
                     && mKeyguardDelegate != null && mKeyguardDelegate.isSecure(mCurrentUserId);
